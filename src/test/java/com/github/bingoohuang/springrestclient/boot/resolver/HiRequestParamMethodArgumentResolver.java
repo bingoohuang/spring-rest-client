847a3a2a286c96097f5ddc25892c0d1b268ec9b1
bingoohuang <bingoohuang@bingoohuangdeMacBook-Pro.local>
2015-06-19 11:23:02 +0800

    enhance HiRequestParamMethodArgumentResolver

--- src/test/java/com/github/bingoohuang/springrestclient/boot/resolver/HiRequestParamMethodArgumentResolver.java
+++ src/test/java/com/github/bingoohuang/springrestclient/boot/resolver/HiRequestParamMethodArgumentResolver.java
@@ -1,6 +1,7 @@
 package com.github.bingoohuang.springrestclient.boot.resolver;
 
 import com.github.bingoohuang.utils.codec.Json;
+import com.google.common.primitives.Primitives;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -34,22 +35,38 @@ public class HiRequestParamMethodArgumentResolver extends RequestParamMethodArgu
     }
 
     private Object tryUnJson(MethodParameter parameter, String str) {
+        Class<?> parameterType = parameter.getParameterType();
+        if (!maybeJsonType(parameterType)) return null;
+
+        return tryParseAsJson(parameter, str, parameterType);
+    }
+
+    private Object tryParseAsJson(MethodParameter parameter, String str, Class<?> parameterType) {
         char c = str.charAt(0);
         boolean maybeJson = c == '{';
-        boolean maybeJsonArray = c == '[';
-        Object obj = null;
         if (maybeJson) {
-            obj = Json.unJson(str, parameter.getParameterType());
-        } else if (maybeJsonArray) {
+            return Json.unJson(str, parameterType);
+        }
+
+        boolean maybeJsonArray = c == '[';
+        if (maybeJsonArray) {
             Type genericParameterType = parameter.getGenericParameterType();
             boolean isGeneric = genericParameterType instanceof ParameterizedType;
             if (isGeneric) {
                 ParameterizedType parameterizedType = (ParameterizedType) genericParameterType;
                 Class<?> argType = (Class) parameterizedType.getActualTypeArguments()[0];
 
-                obj = Json.unJsonArray(str, argType);
+                return Json.unJsonArray(str, argType);
             }
         }
-        return obj;
+        return null;
+    }
+
+    private boolean maybeJsonType(Class<?> parameterType) {
+        if (parameterType.isPrimitive()) return false;
+        if (Primitives.isWrapperType(parameterType)) return false;
+        if (CharSequence.class.isAssignableFrom(parameterType)) return false;
+
+        return true;
     }
 }
