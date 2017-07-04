f010a5e5b7b9d323d10ba2f76bb6a2e869e746b2
bingoo <bingoo.huang@gmail.com>
2017-07-04 11:48:37 +0800

    demo of custom RequestMappingHandlerMapping

--- src/test/java/com/github/bingoohuang/springrestclient/boot/Application.java
+++ src/test/java/com/github/bingoohuang/springrestclient/boot/Application.java
@@ -1,13 +1,19 @@
 package com.github.bingoohuang.springrestclient.boot;
 
 import com.github.bingoohuang.springrest.boot.RestConfiguration;
+import com.github.bingoohuang.springrestclient.annotations.SpringRestClientEnabled;
 import lombok.val;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebMvcRegistrationsAdapter;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.lang.reflect.Method;
 
 @SpringBootApplication
 @Import({RestConfiguration.class})
@@ -32,6 +38,25 @@ public class Application {
         creator.setProxyTargetClass(true);
         return creator;
     }
+
+    @Bean
+    public WebMvcRegistrationsAdapter webMvcRegistrationsAdapter() {
+        return new WebMvcRegistrationsAdapter() {
+            @Override
+            public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
+                return new RequestMappingHandlerMapping() {
+                    @Override
+                    protected void registerHandlerMethod(Object handler, Method method, RequestMappingInfo mapping) {
+                        if (method.getDeclaringClass().isAnnotationPresent(SpringRestClientEnabled.class)) {
+                            return; // by pass SpringRestClientEnabled interface
+                        }
+                        super.registerHandlerMethod(handler, method, mapping);
+                    }
+                };
+            }
+        };
+    }
+
 }
 
 
